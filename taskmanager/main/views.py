b9bf429accacb942809061fa25107cfd1eaa3f8d
Vladislav Zavarzin <vladzav12@gmail.com>
2020-08-18 21:04:08 +0300

    Create first pages

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
 
 # Create your views here.
+
+
+def index(request):
+    tasks = Task.objects.order_by('title')
+    return render(request, 'main/index.html', {'title': 'Главная стрница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверна'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
